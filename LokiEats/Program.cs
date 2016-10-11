e319c5ea5b3caadac2079a8c37eb6e63fe342824
Unknown <jeffreyli@utexas.edu>
2016-10-11 00:13:13 -0700

    Initial code commit

---
+++ LokiEats/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LokiEats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foodToEat = Console.ReadLine();
+            var ingredients = GetIngredients(foodToEat);
+            var isToxic = ingredients.Any(i => i.IsToxic);
+            if (isToxic)
+            {
+                Console.WriteLine(foodToEat + " is toxic to dogs!");
+            }
+            else
+            {
+                Console.WriteLine(foodToEat + " is not toxic to dogs!");
+            }
+            Console.ReadLine();
+        }
+
+        static List<Ingredient> GetIngredients(string food)
+        {
+            return new List<Ingredient>
+            {
+                new Ingredient
+                {
+                    Name = "Garlic",
+                    IsToxic = true
+                }
+            };
+        }
+    }
+}
